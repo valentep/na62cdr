96a7a263491545049be2c339eacdd2aee5636d75
Jonas Kunze <kunze.jonas@gmail.com>
2014-10-08 00:33:12 +0200

    fixed mymaster_cron.pl for multiple instances, kill -9 and mail messages

--- onl_cdr/mymaster_cron.pl
+++ onl_cdr/mymaster_cron.pl
@@ -31,7 +31,7 @@ die "Unknown option $mode" unless ($mode eq "-verbose" || $mode eq "-quiet");
 
 my %controlhash=qw();
 my %wrapperhash=qw();
-my @to_gsm=();
+my @to_m=();
 
 my($setupfile) = "$CCF_ROOT/setup/setup.dat";
 my($usersfile) = "$CCF_ROOT/setup/users.dat";
@@ -39,19 +39,14 @@ my($usersfile) = "$CCF_ROOT/setup/users.dat";
 # Fill in user name and logdir
 my($hostcdr) = getHost();
 my($user) = getUser_mod($usersfile);
-#my $user="na62cdr";
 my($logdir) = getLogsDir("-quiet",$setupfile);
-
-#my $kerb_string = "/usr/bin/k5start -U -f /merger/etc/na62cdr.keytab -b -K 60 -l 61m -- ";
-
-my $kerb_string = "";
 
 ################################################################################
 # Fill in program information here
 # ID as hash key, Commandline, No. of instances
 
 #push @{$controlhash{1}},"onl_cdr/interface_online.pl",1;
-push @{$controlhash{1}},"onl_cdr/submitStage0.pl",1;
+push @{$controlhash{1}},"perl -w $CCF_ROOT/onl_cdr/submitStage0.pl",1;
 push @{$wrapperhash{1}},"onl_cdr/launch_submitStage0.sh",1;
 ###push @{$controlhash{3}},"onl_cdr/complete_online.pl",1; 
 ###push @{$controlhash{4}},"onl_cdr/cleanup_online.pl",1;
@@ -62,8 +57,8 @@ push @{$wrapperhash{1}},"onl_cdr/launch_submitStage0.sh",1;
 
 # check that the log directory is there
 
-foreach (keys %controlhash) {
-    my $dir=$controlhash{$_}[0];
+foreach (keys %wrapperhash) {
+    my $dir=$wrapperhash{$_}[0];
     $dir =~ s/\s//g;
     $dir =~ s/\/.*//g;
     if(!-e "$logdir/$dir") { #permissions not checked...yet
@@ -79,6 +74,7 @@ my @psoutput=();
 get_ps();
 
 foreach my $id (sort keys %controlhash){
+    print "ps_check: $id\n";
     ps_check($id,0);
 }
 
@@ -87,9 +83,9 @@ sleep 2 if((scalar keys %to_stop)>0);
 start_daemons();
 
 if($mode eq "-verbose"){
-    gsm_alarm(@to_gsm);
+    m_alarm(@to_m);
 }else{
-    print "GSM: @to_gsm \n" unless $#to_gsm<0;
+    print "Message: @to_m \n" unless $#to_m<0;
 }
 
 
@@ -100,7 +96,7 @@ sub kill_daemons(){
 	my $short=$controlhash{$_}[0];
 	$short=~ s/^\w+\///; 
 	$short=~s/^([\w\d]+.pl)\s*[\w\s\/\.]*/$1/;
-	next if $short =~ /submitS/;
+#	next if $short =~ /submitS/;
 	print "/usr/bin/killall $short\n";
 	unless(system("/usr/bin/killall -9 $short") ==0){
 	    # killall failed
@@ -112,30 +108,30 @@ sub kill_daemons(){
 		}
 	    }
 	}
-	push @to_gsm,"kill $_ // ";
+	push @to_m,"kill $short // ";
     }
 }
 
 
 sub start_daemons(){
     foreach (keys %to_start){
-	my $log="$logdir/$controlhash{$_}[0]";
+	my $log="$logdir/$wrapperhash{$_}[0]";
 	$log=~ s/\s[\w\/\.]+//g; # cut away options to program
 	$log =~ s/\.pl/\.log/;
-	my $lock="$CCF_ROOT/$controlhash{$_}[0]";
+	my $lock="$CCF_ROOT/$wrapperhash{$_}[0]";
         $lock=~ s/\.pl/\.$hostcdr\.lock/;
 	$lock =~ s/onl_cdr/lockfiles/;
 	print "lock: $lock \n";
 	if(-e $lock){
 	    my $diff=time() - (stat($lock))[10];
-	    push @to_gsm, "LOCK for $_ //" if($diff>7200);
+	    push @to_m, "LOCK for $lock //" if($diff>7200);
 	    next;
 	}
 	system "touch $log" unless(-e $log);
-	print "$kerb_string $CCF_ROOT/$wrapperhash{$_}[0]\n";
-	system "$kerb_string $CCF_ROOT/$wrapperhash{$_}[0]\n";
+	print "$CCF_ROOT/$wrapperhash{$_}[0]\n";
+	system "$CCF_ROOT/$wrapperhash{$_}[0]\n";
 	sleep(2);
-	push @to_gsm,"start $_ // ";
+	push @to_m,"start $wrapperhash{$_}[0] // ";
     }
 }
 
@@ -155,9 +151,12 @@ sub ps_check  {
     $swap=~ s/\.pl\s*[\w\s\/\.]*//;
     $swap=~ s/(\w{0,6})\w*/$1/;
 
+    print "$short\n";
+
     foreach (@psoutput){
 	if(/$commandline/){ # check for full command line
 	    $count++;
+	    print "$_, count++ ($count)\n";
 	}
     }
 
@@ -169,7 +168,8 @@ sub ps_check  {
 	    }
 	}
 	if($swapped>0){
-	    system "/usr/bin/killall -USR1 $short";
+#	    system "/usr/bin/killall -USR1 $short";
+	    print "/usr/bin/killall -USR1 $short\n";
 	    sleep(2);
 	    get_ps();	    
 	    ps_check($_[0],$rec+1);   # recursive ..
@@ -201,29 +201,29 @@ sub get_ps2(){
     @psoutput=`ps -u $user -o pid,command --columns 255 `;
 }   
 
-sub gsm_alarm {
+sub m_alarm {
     my @arg = @_;
     my %arg=qw();
 
-    foreach (@arg){ $arg{$_}=1};
+    foreach (@arg){ $arg{$_}=$_};
     @arg=keys(%arg);
 
     exit if ($#arg<0); # nothing to send
-    if(-e "$CCF_ROOT/sms_address.enable" && $mode eq "-verbose") {
-	my $to_address = `cat $CCF_ROOT/sms_address.enable`;
+    if(-e "$CCF_ROOT/email.enable" && $mode eq "-verbose") {
+	my $to_address = `cat $CCF_ROOT/email.enable`;
 	chomp($to_address);
 	my $subject = $hostcdr.": @arg";
 	open MAIL,"| /usr/sbin/sendmail -t -oi";
-	print MAIL "From: $hostcdr <objsrvvy\@mail.cern.ch>\n";
+	print MAIL "From: $hostcdr <na62.cdr\@cern.ch>\n";
 	print MAIL "To: $to_address \n";
 	print MAIL "Subject: $subject \n";
 	print MAIL "\n";
-	print MAIL "No message body to be sent along with GSM\n";
+	print MAIL "No message body to be sent along\n";
 	close MAIL;
-	print "gsm_alarm: sms sent: $subject\n";
+	print "m_alarm: sent: $subject\n";
     }
     else {
-	print "gsm_alarm: sms disabled\n";
+	print "m_alarm: disabled\n";
     }
 }
 
